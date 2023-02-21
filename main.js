a6b264ef0e37119eb66e498f3271813f0c63b0db
[yolanda lopez] <[yolandalopevi@gmail.com]>
2023-02-21 22:02:32 +0100

    tablas y boton

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const sabadoManana = 30
-const domingoManana = 10
-const sabadoTarde = 30
-const domingoTarde = 60
+const sabadoManana = 30;
+const domingoManana = 10;
+const sabadoTarde = 30;
+const domingoTarde = 60;
 let totalSabado = sabadoManana + sabadoTarde;
 let totalDomingo = domingoManana + domingoTarde;
 let totalFinde = totalSabado + totalDomingo;
 // console.log("sabado",totalSabado)
 // console.log("domingo",totalDomingo)
 
-
-let btn = document.getElementById("btn")
+let btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
-    document.getElementById("total-finde").innerHTML = totalFinde
-} )
+  document.querySelector("#total-finde").innerHTML = totalFinde;
+});
 
 // Manera obosleta, se utiliza actualmente la anterior
 // let btn = document.getElementById("btn")
@@ -21,12 +20,9 @@ btn.addEventListener("click", () => {
 //     document.getElementById("total-finde").innerHTML = totalFinde
 // }
 
-
-
-document.getElementById("num-sab-manana").innerHTML = sabadoManana
-document.getElementById("num-dom-manana").innerHTML = domingoManana
-document.getElementById("num-sab-tardes").innerHTML = sabadoTarde
-document.getElementById("num-dom-tardes").innerHTML = domingoTarde
-document.getElementById("total-sabado").innerHTML = totalSabado
-document.getElementById("total-domingo").innerHTML = totalDomingo
-
+document.getElementById("num-sab-manana").innerHTML = sabadoManana;
+document.getElementById("num-dom-manana").innerHTML = domingoManana;
+document.getElementById("num-sab-tardes").innerHTML = sabadoTarde;
+document.getElementById("num-dom-tardes").innerHTML = domingoTarde;
+document.getElementById("total-sabado").innerHTML = totalSabado;
+document.getElementById("total-domingo").innerHTML = totalDomingo;
